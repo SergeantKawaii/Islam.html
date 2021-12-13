adbacd1ea8d1661829f5465ca45e9db18ad30f85
SergeantKawaii <80927058+SergeantKawaii@users.noreply.github.com>
2021-12-12 23:53:18 -0800

    init

---
+++ test.js
@@ -0,0 +1,34 @@
+//var test = document.getElementsByClassName("test")
+//var anothertest = document.getElementById("Hajj")
+
+//<button type="button"
+//onclick="document.getElementById('demo').innerHTML = Date()">
+//Click me to display Date and Time.</button>;
+
+function closeTab(){
+document.getElementById('buttonSideBar').style.visibility = hidden;
+}
+
+function openTab(){
+  document.getElementsByName('buttonSideBar').style.visibility = visible;
+}
+
+
+
+ //this code is supposed to open and close sidebar, it's just experimentation not actually working idek if it'll work
+ 
+
+
+
+if (closedMenu() === true) {
+return openMenu() }
+else if  (openMenu() === true) {
+return closeMenu()  }
+
+
+
+
+function Timer {
+
+  setTimeout()
+}
